4f0ed5b05776d9e07fbd9a8171fbdd3d6a927c35
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-09-07 16:47:07 +0200

    *add* front / dateUtils

---
+++ src/utils/DateUtils.ts
@@ -0,0 +1,15 @@
+class DateUtils{
+   static secondsToHms(seconds : number) {
+    seconds = Math.floor(seconds);
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = seconds % 60;
+    return [
+      h.toString().padStart(2, '0'),
+      m.toString().padStart(2, '0'),
+      s.toString().padStart(2, '0')
+    ].join(':');
+  }
+}
+
+export default DateUtils
