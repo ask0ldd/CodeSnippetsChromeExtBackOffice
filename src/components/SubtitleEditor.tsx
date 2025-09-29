6879d8d170cfa81cae063a3489a5573859e8e96d
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-09-29 14:17:01 +0200

    *add* front / subtitle editor component

---
+++ src/components/SubtitleEditor.tsx
@@ -0,0 +1,54 @@
+import ISubtitle from "@/interfaces/ISubtitle";
+import { Dispatch, RefObject, SetStateAction } from "react";
+
+export default function SubtitleEditor({text, setText, playerRef, subtitles, setSubtitles} : IProps){
+
+    // !!! should deal with subtitle at the same time position
+    function handleAddSubtitle(e: React.FormEvent){
+        e.preventDefault();
+
+        if(!playerRef.current) return
+
+        const newSub: ISubtitle = { start : playerRef.current.getCurrentTime(), text: text.trim() };
+
+        const existingIndex = subtitles.findIndex(sub => sub.start == newSub.start)
+        
+        if(existingIndex == -1){
+
+        setSubtitles(subs => [...subs, newSub].sort((a, b) => a.start - b.start))
+        return setText("")
+        }
+
+        const newSubtitles = [...subtitles]
+        newSubtitles[existingIndex] = newSub
+        setSubtitles(newSubtitles)
+    }
+
+    return(
+        <form
+            onSubmit={handleAddSubtitle}
+            className="mt-[15px] flex flex-col gap-[10px]"
+        >
+            <div className="flex flex-col">
+            <textarea
+                id="subtitleInput"
+                placeholder="Text to insert at current position"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                required
+                rows={15}
+                className="flex min-h-[40px] border-1 border-gray-300 bg-gray-100 p-[10px] resize-none"
+            />
+            </div>
+            <button type="submit">Add Subtitle</button>
+        </form>
+    )
+}
+
+interface IProps {
+    text : string
+    setText : Dispatch<SetStateAction<string>>
+    subtitles : ISubtitle[]
+    setSubtitles : Dispatch<SetStateAction<ISubtitle[]>>
+    playerRef : RefObject<YT.Player | null>
+}
